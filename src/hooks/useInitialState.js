e67e1fe7db962ac2789f595fad11524bc38438e4
ederson lopez <ederson12f@gmail.com>
2021-11-20 15:31:42 -0500

    implementation of the checkout for purchases

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,18 +1,24 @@
 import { useState } from 'react'
 import initialState from '../initialState'
+
+import { v4 as uuidv4 } from 'uuid'
+
 const useInitialState = () => {
   const [state, setState] = useState(initialState)
 
   const addToCart = product => {
     setState({
       ...state,
-      cart: [...state.cart, product]
+      cart: [...state.cart, {
+        uuid: uuidv4(),
+        ...product
+      }]
     })
   }
-  const removeToCart = product => {
+  const removeToCart = productId => {
     setState({
       ...state,
-      cart: state.cart.filter(items => items.id !== product.id)
+      cart: state.cart.filter(items => items.uuid !== productId)
     })
   }
   return {
