dc55756dd9b1cd0a4e9a24b9d560bf46db8d8e4e
ederson lopez <ederson12f@gmail.com>
2021-11-20 16:12:15 -0500

    state update in addToCart and removeToCart methods

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -7,18 +7,20 @@ const useInitialState = () => {
   const [state, setState] = useState(initialState)
 
   const addToCart = product => {
+    const cart = [...state.cart, {
+      uuid: uuidv4(),
+      ...product
+    }]
     setState({
       ...state,
-      cart: [...state.cart, {
-        uuid: uuidv4(),
-        ...product
-      }]
+      cart
     })
   }
   const removeToCart = productId => {
+    const cart = state.cart.filter(items => items.uuid !== productId)
     setState({
       ...state,
-      cart: state.cart.filter(items => items.uuid !== productId)
+      cart
     })
   }
   return {
