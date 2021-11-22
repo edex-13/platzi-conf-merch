5ea3d53fdab77767b6f685ffafbf6a9d092d8d9f
ederson lopez <ederson12f@gmail.com>
2021-11-21 22:18:35 -0500

    purchase summary for payment

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import '@styles/components/Payment.css'
 
+import { AppContext } from '@context/AppContext'
+
 const Payment = () => {
+  const {
+    state: { cart }
+  } = useContext(AppContext)
   return (
     <div className='Payment'>
       <div className='Payment-content'>
         <h3>Resumen del pedido:</h3>
+        {cart.map((item) => (
+          <div className='Payment-item' key={item.uuid}>
+            <div className='Payment-element'>
+              <h4>{item.title}</h4>
+              <span>$ {item.price}</span>
+            </div>
+          </div>
+        ))}
         <div className='Payment-button'>Boton de pago con Paypal</div>
       </div>
       <div>
